3871245a153f13bc045047721d0b1955bb5c3d02
ranli1992 <ran.li@sjsu.edu>
2017-02-27 09:52:05 -0800

    v3 - all test file can be print out

--- ClassAttribute.java
+++ ClassAttribute.java
@@ -1,23 +1,18 @@
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class ClassAttribute {
 	public List<String> var;
 	public List<String> method;
-	public List<String> otherClass;
 	public ClassAttribute(List<String> var, 
-							List<String> method, List<String> otherClass){
+							List<String> method){
 		this.var = var;
 		this.method = method;
-		this.otherClass = otherClass;
 	}
 	public List<String> getvar () {
 		return var;
 	}
 	public List<String> getmethod () {
 		return method;
-	}
-	public List<String> getOtherClass () {
-		return otherClass;
 	}
 }
--- ParseClass.java
+++ ParseClass.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,12 @@ import java.util.Map;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 
 public class ParseClass {
 	
@@ -20,6 +24,7 @@ public class ParseClass {
 	ClassAttribute ca;
 	String output;
 	List<String> results = new ArrayList<>();
+	List<String> extend_class = new ArrayList<>();
 	
 	public ParseClass(List<CompilationUnit> cuArray, String output) {
 		this.cuArray = cuArray;
@@ -28,6 +33,7 @@ public class ParseClass {
 	
 	public void start() throws IOException {
 		constructClassAttribute();
+		parseField();
 		String yuml_string = results.toString();
 		System.out.println(yuml_string);
 		generateUML.start(yuml_string,output);
@@ -44,23 +50,91 @@ public class ParseClass {
 				for (Node n : nodes) {
 					if (n instanceof ClassOrInterfaceDeclaration) {
 						List<String> var = new ArrayList<>();
-						CI=(ClassOrInterfaceDeclaration) n;
-						n.getNodesByType(FieldDeclaration.class).stream().
-						filter(f -> f.getModifiers().contains(Modifier.PRIVATE) ||f.getModifiers().contains(Modifier.PUBLIC)|| f.getModifiers().contains(Modifier.PROTECTED)). //&& 
-						forEach(f -> var.add(f.toString()));
-						String className= ((ClassOrInterfaceDeclaration) n).getName().toString();
-						ca = new ClassAttribute(var,new ArrayList<String>(), new ArrayList<String>());
+						List<String> method_list = new ArrayList<>();
 						
+						CI=(ClassOrInterfaceDeclaration) n;
+						
+						//parse field without comments
+						List<FieldDeclaration> types = ((ClassOrInterfaceDeclaration) n).getFields();
+						for (FieldDeclaration type : types)
+						{
+						        if (type instanceof FieldDeclaration && (type.getModifiers().contains(Modifier.PRIVATE) ||(type.getModifiers().contains(Modifier.PUBLIC))))
+						        {
+						        	String variable_string = "";
+						        	if (type.getComment() != null) {
+						        		variable_string= type.toString().replace(type.getComment().toString(),"");
+						        	}
+						        	else {
+						        		variable_string = type.toString();
+						        	}
+						        	//System.out.print(variable_string);
+						        	var.add(variable_string);
+						        }
+						}
+						String className= ((ClassOrInterfaceDeclaration) n).getName().toString();
+						
+						//parse constructor
+						 List<TypeDeclaration<?>> typeDeclarations = cu.getTypes();
+						    for (TypeDeclaration typeDec : typeDeclarations) {
+						        List<BodyDeclaration> members = typeDec.getMembers();
+						        if(members != null) {
+						            for (BodyDeclaration member : members) {
+						                if (member instanceof ConstructorDeclaration) {
+						                	String constructor_line = "";
+						                    ConstructorDeclaration constructor = (ConstructorDeclaration) member;
+						                    //System.out.println(constructor.getDeclarationAsString()); 
+						                    if (constructor.getModifiers().toString().contains("PUBLIC")) {
+						                    	constructor_line = "+" + constructor.getNameAsString() +"(";
+						                    	for (int i = 0; i < constructor.getParameters().size() ; i ++) {
+													String[] parameter_string = constructor.getParameter(i).toString().split(" ");
+													constructor_line += parameter_string[1]+":"+parameter_string[0];
+												}
+						                    	constructor_line +=")";
+						                    	method_list.add(constructor_line);
+						                    }
+						                }
+						            }
+						        }
+						    }
 						//parse methods
 						if (!((ClassOrInterfaceDeclaration) n).getMethods().isEmpty()) {
 							for (MethodDeclaration methods: ((ClassOrInterfaceDeclaration) n).getMethods()) {
 								String method_name =methods.getNameAsString();
 								if (!(method_name.startsWith("set")|| method_name.startsWith("get"))) {
-									System.out.println(methods.getNameAsString());
+									if (methods.getModifiers().toString().contains("PUBLIC"))
+									{
+											String method ="";
+											method = "+"+methods.getNameAsString() + "(";
+											for (int i = 0; i < methods.getParameters().size() ; i ++) {
+												String[] parameter_string = methods.getParameter(i).toString().split(" ");
+												method += parameter_string[1]+":"+parameter_string[0];
+											}
+											method +="):"+methods.getType().toString();
+											method = method.replace('[', '(');
+											method = method.replace(']', ')');
+											method_list.add(method);
+											//System.out.println(method);
+											List<Node> child = methods.getChildNodes();
+											for (Node a : child) {
+												String body[] = a.toString().split(" ");
+												for (String current : body) {
+													if (map.containsKey("<<interface>>;"+current) && !((ClassOrInterfaceDeclaration) n).isInterface()) {
+														String addition = "["+className+"]"+"use-.->"+ "[<<interface>>;"+current +"]";
+														if (!results.contains(addition)) {
+															results.add(addition);
+														}
+													}
+												}
+												
+											}
+											
+									}
 								}
 							}
+							
 						}
-						
+						//ref:http://stackoverflow.com/questions/9955859/is-there-a-way-to-get-information-about-the-constructor-using-javaparser-or-some
+						ca = new ClassAttribute(var, method_list);
 						
 						
 						
@@ -68,8 +142,10 @@ public class ParseClass {
 						if (!((ClassOrInterfaceDeclaration) n).getExtendedTypes().isEmpty()) {
 								for (int i = 0; i< (((ClassOrInterfaceDeclaration) n).getExtendedTypes().size()); i++ ) {
 									String extends_name = ((ClassOrInterfaceDeclaration) n).getExtendedTypes(i).toString();
-									extends_name ="["+extends_name+"]-^["+className+"]";
+									extends_name ="["+extends_name+"]^-["+className+"]";
 									results.add(extends_name);
+									extend_class.add(className);
+									
 								}
 						}
 						if (!((ClassOrInterfaceDeclaration) n).getImplementedTypes().isEmpty()) {
@@ -82,9 +158,11 @@ public class ParseClass {
 						}
 						
 						if (((ClassOrInterfaceDeclaration) n).isInterface()) {
-							className="<<Interface>>" +className;
+							className="<<interface>>;"+ className;
 						}
+						//System.out.println(className);
 						map.put(className,ca);
+						
 					}
 				}
 			}
@@ -93,11 +171,14 @@ public class ParseClass {
 	}
 	public void parseField() {
 		for (String a: map.keySet()) {
-			if (map.get(a).getvar() != null && map.get(a).getvar().size() !=0) {
+			//System.out.println(a);
+			if (!a.contains("interface")) {
 			StringBuilder ca_string= new StringBuilder();
+			String addtion ="";
 			ca_string.append('[');
 			ca_string.append(a);
-			ca_string.append('|');
+			if (map.get(a).getvar() != null && map.get(a).getvar().size() !=0) {
+				ca_string.append('|');
 				for (String s: map.get(a).getvar()){
 						s = s.substring(0, s.length() - 1);
 						String[] s_private= s.split(" ");
@@ -123,14 +204,35 @@ public class ParseClass {
 						ca_string.append(";");
 						}
 				}
-				ca_string.append(']');
-				//System.out.println(ca.toString());
-				results.add(0,ca_string.toString());
+			}
+			if (map.get(a).getmethod() != null && map.get(a).getmethod().size() !=0) {
+				ca_string.append("|");
+				for (String s: map.get(a).getmethod()){
+						ca_string.append(s);
+						int start = s.indexOf(':', 0);
+						int end = s.indexOf(')',0);
+						if (end >start) {
+							String otherClass = s.substring((start) + 1, end);
+					
+							if (map.containsKey("<<interface>>;"+otherClass) && !a.contains("<<interface>>") ) {
+								String addition_add ="["+a+"]"+"use-.->"+ "[<<interface>>;"+otherClass +"]";
+								if (!results.contains(addition_add)) {
+									addtion = addition_add;
+								}
+							}
+							if (map.containsKey(otherClass) && !a.contains("<<interface>>")  &&!extend_class.contains(a)) {
+								addtion ="["+a+"]"+"-"+ "["+otherClass +"]";
+							}
+						}
+						ca_string.append(";");
+				}
+			}
+			
+			ca_string.append(']');
+			results.add(0,ca_string.toString());
+			results.add(addtion);
 			}
 		}
-		//for (String i: results) {
-			//System.out.println(results.toString());
-		//}
 		
 	}
 	private String checkModifier(String s) {
@@ -139,9 +241,6 @@ public class ParseClass {
 		}
 		else if (s.contains("public")){
 			return "+";
-		}
-		else if (s.contains("protected")){
-			return "#";
 		}
 		return "";	
 	}
